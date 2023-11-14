2984d177b7f70d7c489c84683edea5eab7c3a46f
amanansari247 <amantech247@gmail.com>
2023-11-14 21:18:32 +0530

    Update UsersList.js

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const UsersList = ({ users }) => {
   return (
-    <div className="container mx-auto my-8">
+    <div className="container mx-auto my-8 mt-24 mb-24">
       <h1 className="text-4xl font-bold mb-4 text-center">All <span className='text-yellow-400'> Stories</span></h1>
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {users.map(user => (
